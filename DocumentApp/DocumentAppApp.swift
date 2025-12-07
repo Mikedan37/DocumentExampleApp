cea632dec617ea27f8475517e34269de9b6004ed
Michael Danylchuk <mn_danylchuk@yahoo.com>
2025-12-06 18:09:41 -0800

    Initial Commit

---
+++ DocumentApp/DocumentAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  DocumentAppApp.swift
+//  DocumentApp
+//
+//  Created by Michael Danylchuk on 12/6/25.
+//
+
+import SwiftUI
+
+@main
+struct DocumentAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
